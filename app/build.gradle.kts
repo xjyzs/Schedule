668ca8a5c354591e8bd8475b3a8f7f3a33ffd0f7
xjyzs <xjyzs135@gmail.com>
2025-11-25 11:57:25 +0800

    feat: 支持滑动切换周数并自动获取token
    - 新增滑动切换周数功能
    - 通过 root 权限自动从微信小程序存储中获取 authorization token
    - 增加 “不再提示” root 权限请求对话框的选项
    - 移动 `parseJson` 函数至 `MainUtil.kt`
    - 新增 `schedule.svg` 应用图标
    - 更新 Gradle 和依赖库版本
    - 降低 `minSdk` 至 33

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.xjyzs.schedule"
         minSdk = 33
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
