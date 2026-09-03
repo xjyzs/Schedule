0311324cc651e4a2922bfea33b951ae1d437890e
雪霁银装素 <158408146+xjyzs@users.noreply.github.com>
2026-09-03 09:33:52 +0800

    可复制对话框内容

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.xjyzs.schedule"
         minSdk = 26
         targetSdk = 37
-        versionCode = 8
-        versionName = "1.2"
+        versionCode = 9
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
